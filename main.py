436c219bbe5759543858ab41a9f61053319f65e6
Holthuizen <45522614+Holthuizen@users.noreply.github.com>
2024-06-17 15:38:39 +0200

    Forked, nothing to see yet
    parking this for when I have some time later

---
+++ main.py
@@ -0,0 +1,4 @@
+#my code for cracking this bitch
+#step one setup a server
+#https://deepmind.google/discover/blog/muzero-mastering-go-chess-shogi-and-atari-without-rules/
+#https://worldmodels.github.io/
